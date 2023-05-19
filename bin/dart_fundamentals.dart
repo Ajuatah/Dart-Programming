515a8434033b4b4512653fb325cf866022a08970
andropov.ajebua <andropov.ajebua@cm.technofex.com>
2023-05-19 18:43:05 +0100

    first commit

---
+++ bin/dart_fundamentals.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fundamentals/dart_fundamentals.dart' as dart_fundamentals;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_fundamentals.calculate()}!');
+}
---
+++ test/dart_fundamentals_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_fundamentals/dart_fundamentals.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
