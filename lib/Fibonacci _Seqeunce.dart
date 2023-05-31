0d0dbbbce5eb74c288bc99e50513f447deeec06a
unknown <yanick>
2023-05-31 07:57:01 -0700

    initial

--- lib/Fibonacci _Seqeunce.dart
+++ lib/Fibonacci _Seqeunce.dart
@@ -1,16 +1,10 @@
-void main() {
-  // Prompt the user for the length of the sequence
-  var n = 10;g
-
-  // Initialize the first two terms
-  var a = 0;
-  var b = 1;
-
-  // Print the first n terms of the sequence
-  for (var i = 0; i < n; i++) {
-    print(a);
-    var c = a + b;
-    a = b;
-    b = c;
-  }
+void main() {/*
+Map <String, int>age={
+  'john':30,
+  'mary':25,
+  'Bob':40,
+}*/
+var details = {'username':'tom','password':'pass@123'};
+details['uid'] = 'u1oo1';
+print(details);
 }
--- lib/NumberSwapper.dart
+++ lib/NumberSwapper.dart
@@ -1,13 +1,33 @@
-// ignore: file_names
-class NumberSwapper {
-  int a;
-  int b;
+import 'dart:io';
 
-  NumberSwapper(this.a, this.b);
+void main() {/*
+//declaring and initialising variable
+  int var1=5;
+  int var2=12;
+  //changing variable value
+  var1=10;
+  var2=20;
+  //print value
+  print("First variable:$var1");
+  print("Second variable:$var2");
+  List myList = new List(4);
+  myList[0]='welcome';
+  myList[1]='to';
+  myList[2]='The';
+  myList[3]='onepercent';
 
-  void swapNumbers() {
-    int temp = a;
-    a = b;
-    b = temp;
-  }
+
+  print(myList[0]);
+  
+  print(myList[1]);
+  
+  print(myList[2]);
+  
+  print(myList[3]);
+  print(myList);*/
+  Map capitals = {"india":"New Delhi","USA":"Washington DC","UK":"London"};
+  print("Capital of india:${capitals['india']}");
+  print("capital of USA:${capitals['USA']}");
+  print("capital of UK:${capitals['UK']}");
+
 }
